3f5a55681d8d5140c5a6d327bb0e62dda78d200a
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2025-03-23 23:11:25 +0100

    Refactor: Rinominata GPT in CalorieBot

--- iWalk/iWalk/Service/CalorieBot.swift
+++ iWalk/iWalk/Service/CalorieBot.swift
@@ -9,26 +9,23 @@ import OpenAI
 import Foundation
 import SwiftDotenv
 
-class GPT: ObservableObject {
-    let openAI: OpenAI
+class CalorieBot {
+    static let shared = CalorieBot()
+    private let openAI: OpenAI
     
-    init() {
-        openAI = OpenAI(apiToken: GPT.loadAPIToken())
+    private init() {
+        openAI = OpenAI(apiToken: CalorieBot.loadAPIToken())
     }
     
     private static func loadAPIToken() -> String {
-        // Trova il file .env nel bundle
         guard let filePath = Bundle.main.path(forResource: ".env", ofType: nil) else {
-            print("File .env non trovato.")
             return ""
         }
         
-        // Carica il file .env e restituisci il token
         do {
             try Dotenv.configure(atPath: filePath)
             return Dotenv["OPENAI_API_KEY"]?.stringValue ?? ""
         } catch {
-            print("Errore nel caricamento del file .env: \(error.localizedDescription)")
             return ""
         }
     }
@@ -36,7 +33,7 @@ class GPT: ObservableObject {
     func getKcal(of food: String) async throws -> Int {
         let query = ChatQuery(
             messages: [
-                .init(role: .system, content: "Rispondi solo con il numero intero totale delle calorie degli alimenti forniti, senza aggiungere mai del testo, di nessun tipo. Se non ci sono alimenti validi o sei incerto, rispondi con 0.")!,
+                .init(role: .system, content: "Rispondi solo con il numero intero totale delle calorie degli alimenti forniti, senza aggiungere mai del testo di nessun tipo. Se non ci sono alimenti validi o sei incerto rispondi con 0")!,
                 .init(role: .user, content: food)!
             ],
             model: .gpt3_5Turbo
@@ -44,13 +41,11 @@ class GPT: ObservableObject {
         
         let result = try await openAI.chats(query: query)
         
-        if let choice = result.choices.first,
-           let message = choice.message.content?.string {
-            return Int(message) ?? 0
-        } else {
-            return -1
+        guard let choice = result.choices.first, let message = choice.message.content?.string else {
+            throw CustomError.noResponse
         }
+        
+        return Int(message) ?? 0
     }
-    
     
 }
