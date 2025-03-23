2455924fd845e444c515ee4f6bc69a8bca6e6408
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2025-03-23 21:57:40 +0100

    Feat: Creata struct HealthData

---
+++ iWalk/iWalk/Model/HealthData.swift
@@ -0,0 +1,15 @@
+//
+//  HealthData.swift
+//  iWalk
+//
+//  Created by Roberto Ambrosino on 10/03/25.
+//
+
+import Foundation
+
+struct HealthData {
+    var date: Date
+    var steps: Int
+    var exerciseMinutes: Int
+    var calories: Int
+}
