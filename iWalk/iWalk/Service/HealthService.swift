e49a83d357f9a3f013991a64905b22c9ffe1f7cb
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2025-03-24 09:59:30 +0100

    Refactor: convertita HealthService in singleton

--- iWalk/iWalk/Service/HealthService.swift
+++ iWalk/iWalk/Service/HealthService.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 import HealthKit
-#if canImport(CreateML)
-import CreateML
-#endif
-import CoreML
-import TabularData
 
 class HealthService {
-    private var healthStore: HKHealthStore? = HKHealthStore()
+    
+    static var shared = HealthService()
+    private var healthStore: HKHealthStore?
+    
+    
+    
+    private init() {
+        healthStore = HKHealthStore()
+    }
     
     
     
@@ -150,51 +153,7 @@ class HealthService {
     }
     
     
-    /*private func removeOutliers(from data: [HealthData]) -> [HealthData] {
-        // Filtra i record con calorie superiori a 30
-        let filteredData = data.filter { $0.calories > 30 }
-
-        // Calcola il rapporto calorie/passi e calorie/minuti di esercizio per ogni record
-        let kcalsPerStep = filteredData.map { Double($0.calories) / Double($0.steps) }
-        let kcalsPerExerciseMinute = filteredData.map { Double($0.calories) / Double($0.exerciseMinutes) }
-
-        // Calcola i quartili per entrambi i rapporti
-        let sortedKcalsPerStep = kcalsPerStep.sorted()
-        let q1Step = percentile(sortedKcalsPerStep, percentile: 25)
-        let q3Step = percentile(sortedKcalsPerStep, percentile: 75)
-
-        let sortedKcalsPerExerciseMinute = kcalsPerExerciseMinute.sorted()
-        let q1Exercise = percentile(sortedKcalsPerExerciseMinute, percentile: 25)
-        let q3Exercise = percentile(sortedKcalsPerExerciseMinute, percentile: 75)
-
-        // Calcola l'IQR e i limiti per gli outlier per entrambi i rapporti
-        let iqrStep = q3Step - q1Step
-        let lowerBoundStep = q1Step - 1.5 * iqrStep
-        let upperBoundStep = q3Step + 1.5 * iqrStep
-
-        let iqrExercise = q3Exercise - q1Exercise
-        let lowerBoundExercise = q1Exercise - 1.5 * iqrExercise
-        let upperBoundExercise = q3Exercise + 1.5 * iqrExercise
-
-        // Filtra i dati per rimuovere gli outlier basati su entrambi i rapporti
-        return filteredData.filter { record in
-            let kcalsPerStep = Double(record.calories) / Double(record.steps)
-            let kcalsPerExerciseMinute = Double(record.calories) / Double(record.exerciseMinutes)
-            return (kcalsPerStep >= lowerBoundStep && kcalsPerStep <= upperBoundStep) &&
-                   (kcalsPerExerciseMinute >= lowerBoundExercise && kcalsPerExerciseMinute <= upperBoundExercise)
-        }
-    }
-
-    private func percentile(_ data: [Double], percentile: Double) -> Double {
-        let index = (percentile / 100.0) * Double(data.count - 1)
-        let lower = data[Int(floor(index))]
-        let upper = data[Int(ceil(index))]
-        return lower + (upper - lower) * (index - floor(index))
-    }*/
     
-
-    
-
     func fetchTodaySteps() async throws -> Int {
         let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         return try await fetchTodayData(for: stepType, unit: .count())
@@ -232,8 +191,6 @@ class HealthService {
             healthStore.execute(query)
         }
     }
-
-
     
     private func combineData(steps: [Date: Int], exerciseMinutes: [Date: Int], calories: [Date: Int]) -> [HealthData] {
         var combinedData: [HealthData] = []
@@ -251,125 +208,4 @@ class HealthService {
         }
     }
     
-    
-    
-    /*private func saveToCSV(data: [HealthData]) throws {
-        // Percorso del file nella directory Documents
-        guard let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            throw CustomError.documentsFolderNotFound
-        }
-            
-        let fileURL = documentsURL.appendingPathComponent("HealthData.csv")
-        
-        // Creazione del contenuto CSV
-        var csvText = "Steps,ExerciseMinutes,Calories\n"
-        
-        for record in data {
-            csvText.append("\(record.steps),\(record.exerciseMinutes),\(record.calories)\n")
-        }
-        
-        // Scrittura del file
-        try csvText.write(to: fileURL, atomically: true, encoding: .utf8)
-    }
-    
-    
-    
-    private func createModel() async throws {
-        #if canImport(CreateML)
-        let fileManager = FileManager.default
-        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let csvURL = documentsDirectory.appendingPathComponent("HealthData.csv")
-        let outputModelURL = documentsDirectory.appendingPathComponent("StepsPredictor.mlmodel")
-        let compiledModelURL = documentsDirectory.appendingPathComponent("StepsPredictor.mlmodelc")
-        
-        // Verifica che il file esista
-        guard fileManager.fileExists(atPath: csvURL.path) else {
-            throw CustomError.csvNotFound
-        }
-
-        // Carica il file CSV in un DataFrame
-        let dataFrame = try DataFrame(contentsOfCSVFile: csvURL)
-        
-        // Crea il modello di regressione
-        let outputModel = try MLLinearRegressor(trainingData: dataFrame, targetColumn: "Steps")
-        
-        // Salva il modello nella cartella Documents
-        try outputModel.write(to: outputModelURL)
-        
-        // Compila il modello
-        let compiledModel = try await MLModel.compileModel(at: outputModelURL)
-        
-        // Salva il modello compilato nella cartella Documents, se ne esiste già uno lo elimina
-        if fileManager.fileExists(atPath: compiledModelURL.path) {
-            try fileManager.removeItem(at: compiledModelURL)
-        }
-        try fileManager.moveItem(at: compiledModel, to: compiledModelURL)
-        #endif
-    }
-    
-    
-    
-    func saveCSVandCreateModel(data: [HealthData]) async throws {
-        try saveToCSV(data: data)
-        try await createModel()
-    }
-    
-
-    
-    private func loadModel() throws -> MLModel {
-        let fileManager = FileManager.default
-        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
-        let modelURL = documentsDirectory.appendingPathComponent("StepsPredictor.mlmodel")
-        let compiledModelURL = documentsDirectory.appendingPathComponent("StepsPredictor.mlmodelc")
-
-        // Se il modello è già compilato, lo si carica direttamente
-        if fileManager.fileExists(atPath: compiledModelURL.path) {
-            return try MLModel(contentsOf: compiledModelURL)
-        }
-
-        // Altrimenti, lo si compila e lo si salva
-        let compiledURL = try MLModel.compileModel(at: modelURL)
-        // fare il fetch e poi il train
-
-        try fileManager.moveItem(at: compiledURL, to: compiledModelURL)
-
-        // Ora carico il modello compilato
-        return try MLModel(contentsOf: compiledModelURL)
-    }
-    
-    
-    
-    private func makePrediction(model: MLModel, exerciseMinutes: Int, calories: Int) throws -> Int {
-        // Prepara i dati di input come dizionario
-        let input = try MLDictionaryFeatureProvider(dictionary: [
-            "ExerciseMinutes": exerciseMinutes,
-            "Calories": calories
-        ])
-        
-        // Esegui la previsione
-        let prediction = try model.prediction(from: input)
-            
-        // Recupera il risultato dalla colonna target (Steps)
-        let steps = prediction.featureValue(for: "Steps")!.doubleValue
-            
-        return Int(steps)
-    }
-    
-    
-
-    func predictSteps(forCalories calories: Int) async throws -> Int {
-        guard calories > 0 else {
-            return 0
-        }
-        
-        let model = try loadModel()
-        
-        let exerciseMinutes = try await fetchTodayExerciseMinutes()
-            
-        let predictedSteps = try makePrediction(model: model, exerciseMinutes: exerciseMinutes, calories: calories)
-                    
-        return predictedSteps
-    }*/
-
 }
