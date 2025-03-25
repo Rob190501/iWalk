62bad74a0183290a9360976ad8baed5c88d86e7d
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2025-03-26 00:59:13 +0100

    Feat: aggiunto supporto codable

--- iWalk/iWalk/Model/HealthData.swift
+++ iWalk/iWalk/Model/HealthData.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
-struct HealthData {
+struct HealthData: Codable {
     var date: Date
     var steps: Int
     var exerciseMinutes: Int
     var calories: Int
+}
+
+extension HealthData: Identifiable {
+    var id: String {
+        "\(date.timeIntervalSince1970)_\(UUID().uuidString)"
+    }
 }
