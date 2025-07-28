3d4aa5110ba51229b7baa5e6604bea3ef755fa98
Rob190501 <95653586+Rob190501@users.noreply.github.com>
2025-07-28 12:22:33 +0200

    Feat: implementata conversione array in dataframe

--- iWalk/iWalk/Model/HealthData.swift
+++ iWalk/iWalk/Model/HealthData.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TabularData
 
 struct HealthData: Codable {
     var date: Date
@@ -18,4 +19,25 @@ extension HealthData: Identifiable {
     var id: String {
         "\(date.timeIntervalSince1970)_\(UUID().uuidString)"
     }
+}
+
+extension Array where Element == HealthData {
+    
+    func toDataFrame() -> DataFrame {
+        guard !self.isEmpty else {
+            return DataFrame()
+        }
+        
+        let steps = self.map { $0.steps }
+        let exerciseMinutes = self.map { $0.exerciseMinutes }
+        let calories = self.map { $0.calories }
+        
+        var dataFrame = DataFrame()
+        dataFrame.append(column: Column(name: "Steps", contents: steps))
+        dataFrame.append(column: Column(name: "ExerciseMinutes", contents: exerciseMinutes))
+        dataFrame.append(column: Column(name: "Calories", contents: calories))
+        
+        return dataFrame
+    }
+    
 }
